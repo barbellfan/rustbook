c5143505b2726862a7ded439aabd4cf0fa27d6c0
Chris Allen <gh.84kjuqg365@gmail.com>
2023-11-23 21:25:57 -0800

    removed the wrong line last time.

--- ch02/ch02_guessing_game/src/main.rs
+++ ch02/ch02_guessing_game/src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     loop {
         println!("Please input your guess.");
 
@@ -22,6 +20,8 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        println!("You guessed: {guess}", );
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
