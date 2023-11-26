76bfe67ba95a41f6c1b8671aa0ea92b5151056c3
Chris Allen <gh.84kjuqg365@gmail.com>
2023-11-25 18:02:25 -0800

    add project for chapter 3.2, data types

---
+++ ch03/ch03_4_datatypes/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x = 2.0; // default is f64.
+    println!("x: {x}");
+
+    let y: f32 = 3.0; // but you can make it an f32.
+    println!("y: {y}")
+    // You could probably print them better so they look like floating points.
+}
