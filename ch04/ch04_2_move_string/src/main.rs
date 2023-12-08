5eda2904904eade447e93bfe6ccb19af2e03faaf
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-08 13:31:59 -0800

    show that simple types don't need clone

--- ch04/ch04_2_move_string/src/main.rs
+++ ch04/ch04_2_move_string/src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2);
+
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y); // this works with out cloning. heap vs stack.
 }
