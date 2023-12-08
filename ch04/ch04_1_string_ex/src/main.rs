37da9e4786a619ba354f007208a513568e9c95ed
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-08 13:23:41 -0800

    add project for move string demo

--- ch04/ch04_1_string_ex/src/main.rs
+++ ch04/ch04_1_string_ex/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut s = String::from("hello");
+    let mut s = String::from("hello"); // s is valid starting here
     s.push_str(", world!");
     println!("{}", s);
-}
+} // s is now out of scope. rust calls drop().
