b705db70cc767868d85a1ae2937a657d6a18a466
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-08 13:28:24 -0800

    fix prev vers using clone

--- ch04/ch04_2_move_string/src/main.rs
+++ ch04/ch04_2_move_string/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     let s1 = String::from("hello");
-    let s2 = s1;
-    println!("{}, world!", s1); // does not compile. attempt to use moved value.
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
