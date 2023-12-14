945ea484184584110d5465eae079622a92b847c9
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-13 19:59:25 -0800

    fix by changing when the last immutable borrow occurs

--- ch04/ch04_8_mut_n_non/src/main.rs
+++ ch04/ch04_8_mut_n_non/src/main.rs
@@ -3,7 +3,9 @@ fn main() {
 
     let r1 = &s; // no problem
     let r2 = &s; // no problem
-    let r3 = &mut s; // BIG PROBLEM
-    println!("{}, {}, and {}", r1, r2, r3);
-    //println!("{}, {}", r1, r2);
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
