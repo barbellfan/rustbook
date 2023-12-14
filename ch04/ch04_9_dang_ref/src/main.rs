0f9b9db5f32b15c9728f28e85c34aedfa205d052
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-13 20:07:39 -0800

    fix by just returning the string fix spelling error in var name

--- ch04/ch04_9_dang_ref/src/main.rs
+++ ch04/ch04_9_dang_ref/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let reference_to_nothging = dangle();
+    let reference_to_nothing = dangle();
+
+    println!("the string: {}", reference_to_nothing);
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
+fn dangle() -> String { // dangle returns a reference to a String
+    let s = String::from("hello"); // s is a new String
 
-    &s
-}
+    s // return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+// Danger!
