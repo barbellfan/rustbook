acdde047f8b07ef3ba2616d93aa6d6b93ad0eddb
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-13 19:50:15 -0800

    fix by not using simultaneous refs

--- ch04/ch04_7_mut_refs/src/main.rs
+++ ch04/ch04_7_mut_refs/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
+    {
+        let r1 = &mut s;
+        println!("{}", r1);
+    }
     let r2 = &mut s;
-    println!("{}, {}", r1, r2);
+    println!("{}", r2);
 }
