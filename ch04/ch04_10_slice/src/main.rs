b4dc48ae5e21cc81593bbb5762d128b2c7fb883e
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-15 21:43:13 -0800

    if range index starts at index 0, you can skip it

--- ch04/ch04_10_slice/src/main.rs
+++ ch04/ch04_10_slice/src/main.rs
@@ -4,5 +4,11 @@ fn main() {
     let world = &s[6..11];
 
     println!("{} {}", hello, world);
+
+    let s2 = String::from("hello");
+    let slice1 = &s2[0..2];
+    let slice2 = &s2[..2];
+
+    println!("slices are equal: {} {}", slice1, slice2);
 }
 
