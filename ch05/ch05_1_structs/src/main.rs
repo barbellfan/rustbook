194792bc8fb5fab1b22d1bfb6b37e80708c0dd4f
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-16 13:22:17 -0800

    tuple structs

--- ch05/ch05_1_structs/src/main.rs
+++ ch05/ch05_1_structs/src/main.rs
@@ -36,6 +36,16 @@ fn main() {
     };
 
     println!("user5 email: {}", user5.email);
+
+    // tuple structs
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("Color black: {} {} {}", black.0, black.1, black.2);
+    println!("Point origin: {} {} {}", origin.0, origin.1, origin.2);
 }
 
 struct User {
