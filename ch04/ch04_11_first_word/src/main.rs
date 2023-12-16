32af64b6d2782e12dbca4ffd2a727a1ef6e8df9d
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-15 21:56:34 -0800

    full ver of first_word()

---
+++ ch04/ch04_11_first_word/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let st = String::from("hello world");
+    let fw = first_word(&st);
+    println!("{}", fw);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
