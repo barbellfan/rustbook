3141a0d033e1d2c3ba83719ced27a6165dced8a8
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-15 22:14:45 -0800

    slice examples

--- ch04/ch04_11_first_word/src/main.rs
+++ ch04/ch04_11_first_word/src/main.rs
@@ -1,11 +1,40 @@
 fn main() {
     let st = String::from("hello world");
     let fw = first_word(&st);
-    //st.clear(); // causes compiler error with borrowing
     println!("{}", fw);
+
+    // first_word works on slices of strings, whether partial or whole
+    let word = first_word(&st[0..6]);
+    println!("partial slice: {}", word);
+
+    let word = first_word(&st[..]);
+    println!("whole slice: {}", word);
+
+    // first_word also works on refrences to string, which are
+    // equivalent to a whole slice of the stringg
+    let word = first_word(&st);
+    println!("reference to the String: {}", word);
+
+    let string_literal = "hello world";
+
+    // first_word works on slices of string literals, whether partial or whole
+    let word = first_word(&string_literal[0..6]);
+    println!("slice of string literal: {}", word);
+    let word = first_word(&string_literal[..]);
+    println!("whole string literal: {}", word);
+
+    // because string literals are string slices already,
+    // this works too, without the slice syntax
+    let word = first_word(string_literal);
+    println!("string literal is a slice: {}", word);
+
+    // slices work with arrays, too
+    let a = [1, 2, 3, 4, 5];
+    let sl = &a[1..3];
+    assert_eq!(sl, &[2, 3]);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
