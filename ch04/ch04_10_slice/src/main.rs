5db61ba5f71b7a753c6753e9e1c7e4e309e08a77
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-15 21:25:14 -0800

    example not using a slice

--- ch04/ch04_10_slice/src/main.rs
+++ ch04/ch04_10_slice/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let phrase = String::from("Hello, world!");
+    let index = first_word(&phrase);
+    println!("Index of end of first word: {}", index);
+}
+
+/// Take a string of words separated by spaces
+/// Return the index of the end of first word in the string.
+/// If no spaces, return the length of the string.
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
