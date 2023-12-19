ed88a2ec9e7188fd02846932dbb92f6a879e635d
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-18 19:37:28 -0800

    a method with the same name as one of the fields.

--- ch05/ch05_3_methods/src/main.rs
+++ ch05/ch05_3_methods/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
 
 fn main() {
@@ -19,4 +23,8 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels",
         rect1.area());
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 }
