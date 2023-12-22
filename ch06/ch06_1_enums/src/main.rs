ab7e7a153536a580901b7cb828e85c50a8a90763
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-22 14:38:33 -0800

    enums can have different associated data

--- ch06/ch06_1_enums/src/main.rs
+++ ch06/ch06_1_enums/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     #[derive(Debug)]
     enum IpAddr {
-        V4(String),
+        V4(u8, u8, u8, u8),
         V6(String),
     }
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(10, 0, 0, 0);
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("home ip addr: {:?}", home);
