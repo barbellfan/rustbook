33097e01eb29a4c0febf17ac26c5cca8de4921e4
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-22 14:35:59 -0800

    fix white space

--- ch06/ch06_1_enums/src/main.rs
+++ ch06/ch06_1_enums/src/main.rs
@@ -4,7 +4,8 @@ fn main() {
         V4(String),
         V6(String),
     }
-        let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("home ip addr: {:?}", home);
