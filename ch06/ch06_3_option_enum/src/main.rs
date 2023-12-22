59b47478ee87286c0fea82331acaedd5eb9c69e3
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-22 15:09:48 -0800

    using the Option enum

--- ch06/ch06_3_option_enum/src/main.rs
+++ ch06/ch06_3_option_enum/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let some_number = Some(5);
+    let some_char = Some('e');
+    let absent_number: Option<i32> = None;
+
+    println!("some_number: {:?}", some_number);
+    println!("some_char: {:?}", some_char);
+    println!("absent_number: {:?}", absent_number);
 }
