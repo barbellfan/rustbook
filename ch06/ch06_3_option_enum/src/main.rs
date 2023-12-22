5a7c3b289055d59d94d1aad55ebe1f5d75b5efcd
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-22 15:15:29 -0800

    added comment showing code that won't compile. yet.

--- ch06/ch06_3_option_enum/src/main.rs
+++ ch06/ch06_3_option_enum/src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     println!("some_number: {:?}", some_number);
     println!("some_char: {:?}", some_char);
     println!("absent_number: {:?}", absent_number);
+
+    /* This won't compile since y is not the same type as x.
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    let sum = x + y;
+    */
 }
