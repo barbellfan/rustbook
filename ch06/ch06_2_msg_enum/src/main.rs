5b0eff09665913e59f96c12273084f3b15169f34
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-22 14:53:43 -0800

    example enum with 4 variants

--- ch06/ch06_2_msg_enum/src/main.rs
+++ ch06/ch06_2_msg_enum/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    #[allow(unused)]
+    enum Message {
+        Quit,
+        Move {x: i32, y: i32},
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // method body
+        }
+    }
+
+    let w = Message::Write(String::from("hello"));
+    w.call();
+
+    let q = Message::Quit;
+    q.call();
+
+    let m = Message::Move { x: 2, y: 3 };
+    m.call();
+
+    let c = Message::ChangeColor(5, 6, 7);
+    c.call();
 }
+
