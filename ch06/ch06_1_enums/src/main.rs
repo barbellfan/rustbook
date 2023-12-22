b9a57a0d424a7638c4dd784fb42e40718f8cb66f
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-22 14:34:55 -0800

    use enum with assoicated String vals

--- ch06/ch06_1_enums/src/main.rs
+++ ch06/ch06_1_enums/src/main.rs
@@ -1,25 +1,13 @@
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    println!("home ip addr: {} {:?}", home.address, home.kind);
-    println!("loopback ip addr: {} {:?}", loopback.address, loopback.kind);
+    println!("home ip addr: {:?}", home);
+    println!("loopback ip addr: {:?}", loopback);
 }
 
 #[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
