5e6dec0af6562ec2dd7485adbcbfdda9473745f4
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-22 19:10:32 -0800

    doing nothing on the default case with the unit value.

--- ch06/ch06_6_match_dice/src/main.rs
+++ ch06/ch06_6_match_dice/src/main.rs
@@ -3,12 +3,8 @@ fn main() {
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
-        _ => reroll(),
+        _ => (),
     }
-}
-
-fn reroll() {
-    println!("reroll");
 }
 
 fn remove_fancy_hat() {
