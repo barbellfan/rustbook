73117126b29920673970d47e3c1ca9c18df9e679
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-24 20:49:38 -0800

    using if let with None. Not in the book, just wanted to try it out.

--- ch06/ch06_7_iflet/src/main.rs
+++ ch06/ch06_7_iflet/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     if let Some(max) = config_max {
         println!("The maximum is configured to be {}", max);
     }
+
+    let un_conf: Option<u8> = Option::None;
+    if let None = un_conf {
+        println!("un_conf = {:?}", un_conf);
+    }
 }
