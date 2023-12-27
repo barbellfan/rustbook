9537459c1873f3101d52a89b6b4adfa01ea5241e
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-26 18:12:20 -0800

    using super in mod paths

--- ch07/ch07_1_restaurant/src/lib.rs
+++ ch07/ch07_1_restaurant/src/lib.rs
@@ -15,6 +15,16 @@ mod front_of_house {
     }
 }
 
+#[allow(unused)]
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
 pub fn eat_at_restaurant() {
     // absolute path
     crate::front_of_house::hosting::add_to_waitlist();
@@ -22,3 +32,5 @@ pub fn eat_at_restaurant() {
     // relative path
     front_of_house::hosting::add_to_waitlist();
 }
+
+fn deliver_order() {}
