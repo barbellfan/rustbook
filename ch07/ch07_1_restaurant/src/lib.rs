581633c05495c539050dfa76e5e6051bf8f88727
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-26 18:20:32 -0800

    enums are all public

--- ch07/ch07_1_restaurant/src/lib.rs
+++ ch07/ch07_1_restaurant/src/lib.rs
@@ -37,8 +37,14 @@ mod back_of_house {
             }
         }
     }
+
+    pub enum  Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
+#[allow(unused)]
 pub fn eat_at_restaurant() {
     // absolute path
     crate::front_of_house::hosting::add_to_waitlist();
@@ -55,6 +61,9 @@ pub fn eat_at_restaurant() {
     // the next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
     // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
 fn deliver_order() {}
