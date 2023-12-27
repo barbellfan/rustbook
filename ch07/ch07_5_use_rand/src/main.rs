d37b2adbfaa1f79c2387d00b226f6b3e92631825
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-27 15:15:16 -0800

    combining paths with self adjust earlier use statement to avoid using io twice.

--- ch07/ch07_5_use_rand/src/main.rs
+++ ch07/ch07_5_use_rand/src/main.rs
@@ -2,7 +2,11 @@ use rand::Rng;
 
 // using nested paths when importing
 #[allow(unused_imports)]
-use std::{cmp::Ordering, io};
+use std::{cmp::Ordering, array};
+
+// using self to combine paths
+#[allow(unused_imports)]
+use std::io::{self, Write};
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
