4caf12cba0ca29a0473671241d93f5b763dbbef8
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-27 15:12:21 -0800

    using nested paths when imporing

--- ch07/ch07_5_use_rand/src/main.rs
+++ ch07/ch07_5_use_rand/src/main.rs
@@ -1,5 +1,9 @@
 use rand::Rng;
 
+// using nested paths when importing
+#[allow(unused_imports)]
+use std::{cmp::Ordering, io};
+
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("secret number: {}", secret_number);
