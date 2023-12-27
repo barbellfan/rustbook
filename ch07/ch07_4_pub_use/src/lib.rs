e60566f0177fe733e1c6eb3c4290115683f7643c
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-27 14:41:32 -0800

    demo use of pub use

--- ch07/ch07_4_pub_use/src/lib.rs
+++ ch07/ch07_4_pub_use/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[allow(unused)]
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub use crate::front_of_house::hosting;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
