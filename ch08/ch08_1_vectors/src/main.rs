7816804e1e57d428f43577a28d6c0634029a517f
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-28 14:52:52 -0800

    initializing a vector with vec!

--- ch08/ch08_1_vectors/src/main.rs
+++ ch08/ch08_1_vectors/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     creating_a_new_vector();
+    initializing_a_new_vector();
 }
 
 fn creating_a_new_vector() {
     let v: Vec<i32> = Vec::new();
+    println!("new Vec: {:?}", v);
+}
+
+fn initializing_a_new_vector() {
+    let v = vec![1, 2, 3];
     println!("new Vec: {:?}", v);
 }
