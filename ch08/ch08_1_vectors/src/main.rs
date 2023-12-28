eacde1ae1ed9c3a870885f6f3cee27878a258462
Chris Allen <gh.84kjuqg365@gmail.com>
2023-12-28 14:54:41 -0800

    updating a vector

--- ch08/ch08_1_vectors/src/main.rs
+++ ch08/ch08_1_vectors/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     creating_a_new_vector();
     initializing_a_new_vector();
+    updating_a_vector();
 }
 
 fn creating_a_new_vector() {
@@ -11,4 +12,15 @@ fn creating_a_new_vector() {
 fn initializing_a_new_vector() {
     let v = vec![1, 2, 3];
     println!("new Vec: {:?}", v);
+}
+
+fn updating_a_vector() {
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    println!("updated vector: {:?}", v);
 }
