516f89a7426f6ca77fd4c97c697c8359bdb58176
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-06 14:12:37 -0800

    getting values from a hashmap

--- ch08/ch08_3_hashmaps/src/main.rs
+++ ch08/ch08_3_hashmaps/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 fn main() {
     creating_new_hashmap();
+    getting_values_from_hashmaps();
 }
 
 fn creating_new_hashmap() {
@@ -11,4 +12,22 @@ fn creating_new_hashmap() {
     scores.insert(String::from("Yellow"), 50);
 
     println!("scores: {scores:?}");
+}
+
+fn getting_values_from_hashmaps() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("using get()");
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("score for {team_name}: {score}");
+
+    println!("iterating over all values");
+    for (key, value) in &scores {
+        println!("{key}: {value}")
+    }
 }
