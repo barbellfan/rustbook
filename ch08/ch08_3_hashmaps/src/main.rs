4bae0af75b675d1713e474aa3edc1bba6090ba69
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-06 14:19:50 -0800

    hashmap and ownership example

--- ch08/ch08_3_hashmaps/src/main.rs
+++ ch08/ch08_3_hashmaps/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 fn main() {
     creating_new_hashmap();
     getting_values_from_hashmaps();
+    hashmaps_and_ownership();
 }
 
 fn creating_new_hashmap() {
@@ -30,4 +31,21 @@ fn getting_values_from_hashmaps() {
     for (key, value) in &scores {
         println!("{key}: {value}")
     }
+}
+
+fn hashmaps_and_ownership() {
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    println!("map: {map:?}");
+
+    // these fail at compilation with "borrow of moved value"
+    //println!("field_name: {field_name}");
+    //println!("field_value: {field_value}");
+
+    // Hashmaps own their variables. Unless you insert a reference.
+    // Then the reference must be valid as long as the Hashmap is valid.
 }
