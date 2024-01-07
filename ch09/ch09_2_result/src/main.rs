6bc01f6fd56f8093169c2bab332620844df54cd7
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-07 15:09:50 -0800

    move code to a function. comment it out so other examples can run. match .on different errors

--- ch09/ch09_2_result/src/main.rs
+++ ch09/ch09_2_result/src/main.rs
@@ -1,11 +1,36 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
+    //_recoverable_errors_with_result();
+    matching_on_different_errors();
+}
+
+fn _recoverable_errors_with_result() {
     let greeting_file_result = File::open("hello.txt");
 
     let greeting_file = match  greeting_file_result {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    println!("greeting file: {:?}", greeting_file);
+}
+
+fn matching_on_different_errors() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        }
     };
 
     println!("greeting file: {:?}", greeting_file);
