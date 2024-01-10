70793937d8bd6e6c1d316c9f42b007a2b737ea9a
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-09 19:27:37 -0800

    using generics in struct defs

--- ch10/ch10_2_generics/src/main.rs
+++ ch10/ch10_2_generics/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     generic_in_fn_def();
+    generic_in_struct_def();
+    generic_in_struct_def_2();
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
@@ -24,4 +26,38 @@ fn generic_in_fn_def() {
 
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn generic_in_struct_def() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.01, y: 4.01 };
+    //let wont_work = Point { x: 5, y: 4.0 }; // types must match
+
+    println!("integer.x: {}", integer.x);
+    println!("integer.y : {}", integer.y);
+    println!("float.x: {}", float.x);
+    println!("float.y: {}", float.y);
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+fn generic_in_struct_def_2() {
+    let both_integer = Point2 { x: 5, y: 10 };
+    let both_float = Point2 { x: 1.01, y: 4.01 };
+    let integer_and_float = Point2 { x: 5, y: 4.01 };
+
+    println!("integer.x: {}", both_integer.x);
+    println!("integer.y : {}", both_integer.y);
+    println!("both_float.x: {}", both_float.x);
+    println!("both_float.y: {}", both_float.y);
+    println!("integer_and_float.x: {}", integer_and_float.x);
+    println!("integer_and_float.y: {}", integer_and_float.y);
 }
