25abaf288104d40b942ba7040a267ffbcff66c67
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-09 19:58:43 -0800

    generic parameters in impl vs declared as params

--- ch10/ch10_2_generics/src/main.rs
+++ ch10/ch10_2_generics/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     generic_in_struct_def();
     generic_in_struct_def_2();
     generic_in_method_def();
+    generic_mixup();
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
@@ -81,4 +82,27 @@ fn generic_in_method_def() {
 
     let p2 = Point { x: 10.0, y: 20.0 };
     println!("distance: {}", p2.distance_from_origin());
+}
+
+struct Point3<X1,Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl <X1, Y1> Point3<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point3<X2, Y2>) -> Point3<X1, Y2> {
+        Point3 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn generic_mixup() {
+    let p1 = Point3 { x: 5, y: 10.4 };
+    let p2 = Point3 {x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
