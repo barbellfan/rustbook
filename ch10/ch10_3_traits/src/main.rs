897b76502ed4b999bcbc77852c2a186b49ef3805
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-10 21:29:18 -0800

    create a trait and impl it on a couple of structs

--- ch10/ch10_3_traits/src/main.rs
+++ ch10/ch10_3_traits/src/main.rs
@@ -1,3 +1,22 @@
+mod aggregator;
+use aggregator::{Summary, NewsArticle, Tweet};
+
 fn main() {
-    println!("Hello, world!");
+    let na = NewsArticle {
+        headline: String::from("headline"),
+        location: String::from("location"),
+        author: String::from("author"),
+        content: String::from("content content content"),
+    };
+
+    println!("summary of news article: {}", na.summarize());
+
+    let tw = Tweet {
+        username: String::from("@tweeter"),
+        content: String::from("a tweet"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("summary of tweet: {}", tw.summarize());
 }
