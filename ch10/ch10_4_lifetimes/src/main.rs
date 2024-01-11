03908edca0222ce66a75018e195964d3fb81c0bc
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-11 10:04:37 -0800

    does not compile because of lifetime issue in longest()

--- ch10/ch10_4_lifetimes/src/main.rs
+++ ch10/ch10_4_lifetimes/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     prevent_dangling_references();
+    generic_lifetimes_in_functions();
 }
 
 fn prevent_dangling_references() {
@@ -8,4 +9,20 @@ fn prevent_dangling_references() {
     let r = &x;
 
     println!("r: {}", r);
+}
+
+fn generic_lifetimes_in_functions() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
