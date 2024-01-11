8a735551262689ce09c5a64cb5ce7ac77920c520
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-11 09:56:21 -0800

    dangling reference example. won't compile.

--- ch10/ch10_4_lifetimes/src/main.rs
+++ ch10/ch10_4_lifetimes/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    prevent_dangling_references();
+}
+
+fn prevent_dangling_references() {
+    let r;
+
+    {
+        let x = 5;
+        r = &x;
+    }
+
+    println!("r: {}", r);
 }
