1a8b917cff74331aa48a524287b2d5ee2cdc3bb4
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-11 10:18:02 -0800

    lifetime example where compilation fails wnen string2 is out of scope

--- ch10/ch10_4_lifetimes/src/main.rs
+++ ch10/ch10_4_lifetimes/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     prevent_dangling_references();
     generic_lifetimes_in_functions();
+    different_lifetimes();
+    different_lifetimes_2();
 }
 
 fn prevent_dangling_references() {
@@ -25,4 +27,25 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     } else {
         y
     }
+}
+
+fn different_lifetimes() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+}
+
+fn different_lifetimes_2() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result); // works here
+    }
+    //println!("The longest string is {}", result); // but not here, since string2 is out of scope
 }
