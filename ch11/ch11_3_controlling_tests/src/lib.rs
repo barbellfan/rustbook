1921f8d80958c9afbcc643e44df60764523f4eb8
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-11 17:05:19 -0800

    running subsets of tests

--- ch11/ch11_3_controlling_tests/src/lib.rs
+++ ch11/ch11_3_controlling_tests/src/lib.rs
@@ -3,6 +3,10 @@ pub fn prints_and_returns_10(a: i32) ->i32 {
     10
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,10 +17,29 @@ mod tests {
         assert_eq!(10, value);
     }
 
+    // run this ignored test by running: cargo test -- --ignored
     #[test]
     #[ignore]
     fn this_test_will_fail() {
         let value = prints_and_returns_10(8);
         assert_eq!(5, value);
     }
+
+    // run the tests that start with `add` by running: cargo test add
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    // run this test by itself by running: cargo test one_hundred
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
 }
