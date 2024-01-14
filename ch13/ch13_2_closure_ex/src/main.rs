26e3198577b92dc10bb1b03387acb7c17de54ebd
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-14 13:08:20 -0800

    expensive closure example

--- ch13/ch13_2_closure_ex/src/main.rs
+++ ch13/ch13_2_closure_ex/src/main.rs
@@ -1,3 +1,14 @@
+use std::{thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    let ex_cl_res = expensive_closure(5);
+
+    println!("Result from expensive closure: {}", ex_cl_res);
 }
+
