9653d1528014159748f20e0ff3f0c6e453cb00ca
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-14 15:23:31 -0800

    capturing references or moving ownership

--- ch13/ch13_2_closure_ex/src/main.rs
+++ ch13/ch13_2_closure_ex/src/main.rs
@@ -1,8 +1,11 @@
-use std::{thread, time::Duration};
+use std::{thread, time::Duration, vec};
 
 fn main() {
     exp_closure();
     various_closures();
+    sharing_immutable_refs();
+    sharing_mutable_refs();
+    force_closure_ownership();
 }
 
 fn exp_closure() {
@@ -46,4 +49,47 @@ fn various_closures() {
     let s = example_closure(String::from("hello"));
     //let n = example_closure(5); // this won't compile because the previous line set the data type of the closure.
     println!("example closure result: {s}");
+}
+
+fn sharing_immutable_refs() {
+    println!("Immutable references can be shared.");
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+}
+
+fn sharing_mutable_refs() {
+    println!("Mutable references can be shared too.");
+
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+    // cannot borrow `list` as immutable because it is also borrowed as mutable
+    //println!("After defining closure: {:?}", list);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+}
+
+fn force_closure_ownership() {
+    println!("Moving ownership");
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    // move keyword is required here, since list is going to another thread.
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    // cannot use list here:
+    // borrow of moved value
+    //println!("Can I use list here?: {:?}", list);
 }
