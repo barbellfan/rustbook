acfe47b5be31473433095f0bfe63b34aa53f84a2
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-15 09:59:59 -0800

    simple iterator example

--- ch13/ch13_3_iterators/src/main.rs
+++ ch13/ch13_3_iterators/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    iterator_ex();
+}
+
+fn iterator_ex() {
+    println!("iterator example");
+
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter(); // lazy iterator does nothing until you use it.
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
 }
