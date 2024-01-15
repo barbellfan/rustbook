8fdc1405e08a103b77efa1299aa2a330bdfee827
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-15 10:25:01 -0800

    call a consuming adaptor on an iterator to make it do stuff.

--- ch13/ch13_3_iterators/src/main.rs
+++ ch13/ch13_3_iterators/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     iterator_ex();
+    methods_that_produce_other_iterators();
 }
 
 fn iterator_ex() {
@@ -14,6 +15,14 @@ fn iterator_ex() {
     }
 }
 
+fn methods_that_produce_other_iterators() {
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    //v1.iter().map(|x| x + 1); // does nothing! iterator returned by map is not used.
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+}
+
 #[test]
 fn iterator_demonstration() {
     let v1 = vec![1, 2, 3];
@@ -24,4 +33,15 @@ fn iterator_demonstration() {
     assert_eq!(v1_iter.next(), Some(&2));
     assert_eq!(v1_iter.next(), Some(&3));
     assert_eq!(v1_iter.next(), None);
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum(); // can't use v1_iter after this call
+
+    assert_eq!(total, 6);
 }
