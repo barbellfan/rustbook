912255841eebb8dcaec59e9c0d5b8e5669a635a0
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-18 20:47:27 -0800

    confusing example of a recursive enum using Box<T>

--- ch15/ch15_1_boxt/src/main.rs
+++ ch15/ch15_1_boxt/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
     using_box_t();
+    recursive_types();
 }
 
 fn using_box_t() {
     let b = Box::new(5);
     println!("b = {}", b);
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+// Why is this needed? I never needed it before when using a custom enum.
+use crate::List::{Cons, Nil};
+
+fn recursive_types() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("list = {:?}", list);
 }
