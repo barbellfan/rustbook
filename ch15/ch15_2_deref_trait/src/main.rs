5a8cafb099e7908a7bea65f139fc8e423b29b952
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-21 13:49:37 -0800

    using box T like a reference

--- ch15/ch15_2_deref_trait/src/main.rs
+++ ch15/ch15_2_deref_trait/src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
     follow_pointer_to_value();
+    use_boxt_like_a_reference();
 }
 
 fn follow_pointer_to_value() {
     let x = 5;
     let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn use_boxt_like_a_reference() {
+    let x = 5;
+    let y = Box::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
