0b3caf94a1d867d42161fec21cf16f1e7ce681b7
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-21 15:32:46 -0800

    struc doesn't know how to deref custom struct

--- ch15/ch15_2_deref_trait/src/main.rs
+++ ch15/ch15_2_deref_trait/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     follow_pointer_to_value();
     use_boxt_like_a_reference();
+    define_our_own_smart_pointer();
 }
 
 fn follow_pointer_to_value() {
@@ -14,6 +15,22 @@ fn follow_pointer_to_value() {
 fn use_boxt_like_a_reference() {
     let x = 5;
     let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn define_our_own_smart_pointer() {
+    let x = 5;
+    let y = MyBox::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
