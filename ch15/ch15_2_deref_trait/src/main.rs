4294df02d3ee2097d673bb9a03e480abfa615df2
Chris Allen <gh.84kjuqg365@gmail.com>
2024-01-21 13:45:19 -0800

    using the dereference operator

--- ch15/ch15_2_deref_trait/src/main.rs
+++ ch15/ch15_2_deref_trait/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    follow_pointer_to_value();
+}
+
+fn follow_pointer_to_value() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
