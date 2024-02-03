698244c4532ed789b510f542e2e2e1e45f81dabb
Chris Allen <gh.84kjuqg365@gmail.com>
2024-02-03 13:41:56 -0800

    visualizing strong and weak counts

--- ch15/ch15_6a_prev_ref_cycle/src/main.rs
+++ ch15/ch15_6a_prev_ref_cycle/src/main.rs
@@ -11,6 +11,7 @@ struct Node {
 
 fn main() {
     add_ref_from_child_to_parent();
+    visualize_strong_and_weak_counts();
 }
 
 fn add_ref_from_child_to_parent() {
@@ -31,4 +32,48 @@ fn add_ref_from_child_to_parent() {
     *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+}
+
+fn visualize_strong_and_weak_counts() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
 }
