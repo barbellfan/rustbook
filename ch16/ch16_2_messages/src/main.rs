e42786debc062d284fafae57e034adbb26c1b1b2
Chris Allen <gh.84kjuqg365@gmail.com>
2024-02-10 13:33:22 -0800

    create the transmitter

--- ch16/ch16_2_messages/src/main.rs
+++ ch16/ch16_2_messages/src/main.rs
@@ -1,6 +1,11 @@
-use std::sync::mpsc;
+use std::{sync::mpsc, thread};
 
 fn main() {
     // mpsc = Multiple Producer, Single Consumer
     let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
 }
