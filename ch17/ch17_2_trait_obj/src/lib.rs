39aab2f875157bacc39c92745b8ca6ba02b009ea
Chris Allen <gh.84kjuqg365@gmail.com>
2024-02-11 12:52:19 -0800

    trait object example

--- ch17/ch17_2_trait_obj/src/lib.rs
+++ ch17/ch17_2_trait_obj/src/lib.rs
@@ -1,5 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +19,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn to_test() {
+        assert_eq!(4, 4);
     }
 }
