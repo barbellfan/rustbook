166de61d3158a8ef639c51edba51bc4091e263ed
Chris Allen <gh.84kjuqg365@gmail.com>
2024-02-19 20:35:23 -0800

    matching literals example

---
+++ ch18/ch18_3_matching_literals/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    matching_lilterals();
+}
+
+fn matching_lilterals() {
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
