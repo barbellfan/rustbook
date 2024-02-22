9ec02287d2567a04b0bf5dbb229c69ddd56e5add
Chris Allen <gh.84kjuqg365@gmail.com>
2024-02-21 20:37:24 -0800

    example of destructuring an enum

--- ch18/ch18_3_matching_literals/src/main.rs
+++ ch18/ch18_3_matching_literals/src/main.rs
@@ -1,3 +1,5 @@
+use std::os::unix::raw::gid_t;
+
 fn main() {
     matching_literals();
     matching_named_variables();
@@ -5,6 +7,7 @@ fn main() {
     matching_ranges();
     destructuring_struct_1();
     destructuring_struct_2();
+    destructuring_enums();
 }
 
 fn matching_literals() {
@@ -87,6 +90,32 @@ fn destructuring_struct_2() {
         Point { x: 0, y } => println!("On the y axis at {y}"),
         Point { x, y } => {
             println!("On neither axis: ({x}, {y})");
+        }
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn destructuring_enums() {
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variante has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message : {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}");
         }
     }
 }
