6af2e1b4a740301155516ce44f746548260b7b61
Chris Allen <gh.84kjuqg365@gmail.com>
2024-04-06 19:37:15 -0700

    create row pointers from a reference

--- ch19/ch19_1_unsafe_rust/src/main.rs
+++ ch19/ch19_1_unsafe_rust/src/main.rs
@@ -1,3 +1,23 @@
+use std::any::type_name;
+
 fn main() {
-    println!("Hello, world!");
+    raw_pointers_from_references();
+}
+
+fn raw_pointers_from_references()
+{
+    let mut num = 5;
+
+    // no unsafe needed
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    // not in the book, but I wanted to print out the type
+    print_type_of(&num); // prints i32
+    print_type_of(&r1); // prints *const i32
+    print_type_of(&r2); // *mut i32
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", type_name::<T>())
 }
