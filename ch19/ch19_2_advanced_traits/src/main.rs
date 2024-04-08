24607e25739594450ede528c40f2960489472c12
Chris Allen <gh.84kjuqg365@gmail.com>
2024-04-07 19:23:43 -0700

    add another example of generic type params etc

--- ch19/ch19_2_advanced_traits/src/main.rs
+++ ch19/ch19_2_advanced_traits/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     default_generic_type_params_and_op_overload();
+    default_generic_type_params_and_op_overload_2();
 }
 
 use std::ops::Add;
@@ -26,4 +27,23 @@ fn default_generic_type_params_and_op_overload() {
         Point { x: 1, y:0 } + Point { x: 2, y: 3},
         Point { x: 3, y: 3}
     );
+}
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+fn default_generic_type_params_and_op_overload_2() {
+    let mi = Millimeters(100);
+    let me = Meters(2);
+
+    let added = mi.add(me);
+    println!("meters + millimeters: {}", added.0);
 }
