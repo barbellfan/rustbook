f91ee7c57ce95a1c0b9c051480d95782f1e8f822
Chris Allen <gh.84kjuqg365@gmail.com>
2024-04-07 19:08:06 -0700

    demo of a default generic type parameter and operator overloading

--- ch19/ch19_2_advanced_traits/src/main.rs
+++ ch19/ch19_2_advanced_traits/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    default_generic_type_params_and_op_overload();
+}
+
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+fn default_generic_type_params_and_op_overload() {
+    assert_eq!(
+        Point { x: 1, y:0 } + Point { x: 2, y: 3},
+        Point { x: 3, y: 3}
+    );
 }
