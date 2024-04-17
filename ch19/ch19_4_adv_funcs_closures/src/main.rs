0ea86a8f7d29cb7c14a8968665188d2b496ed30a
Chris Allen <gh.84kjuqg365@gmail.com>
2024-04-16 20:42:14 -0700

    example of returning a closure

--- ch19/ch19_4_adv_funcs_closures/src/main.rs
+++ ch19/ch19_4_adv_funcs_closures/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     function_pointers();
+    returning_closures();
 }
 
 fn function_pointers() {
@@ -14,4 +15,17 @@ fn function_pointers() {
     let answer = do_twice(add_one, 5);
 
     println!("The answer is: {}", answer);
+}
+
+fn returning_closures() {
+    /* does not compile since you can't return a bare closure
+    fn returns_closure() -> dyn Fn(i32) -> i32 {
+        |x| x + 1
+    }
+    */
+
+    // this work because you are returning a Box object
+    fn _returns_closure() -> Box<dyn Fn(i32) -> i32> {
+        Box::new(|x| x + 1)
+    }
 }
