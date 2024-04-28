c68dcea284564870699a940dad396189737982f8
Chris Allen <gh.84kjuqg365@gmail.com>
2024-04-27 19:51:53 -0700

    check for get request and root. return nothing if not that.

--- ch20/hello/src/main.rs
+++ ch20/hello/src/main.rs
@@ -17,22 +17,18 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    /*
-    let paths = fs::read_dir("./").unwrap();
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display());
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+        let contents = fs::read_to_string("ch20/hello/hello.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        // some other request
     }
-    */
-    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
-    let contents = fs::read_to_string("ch20/hello/hello.html").unwrap();
-    let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
 }
