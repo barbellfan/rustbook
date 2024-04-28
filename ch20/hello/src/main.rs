65536b2d1ec1e83c7e68baec64bda43c4023b26d
Chris Allen <gh.84kjuqg365@gmail.com>
2024-04-27 20:08:36 -0700

    redirect any other requests to the 404 page

--- ch20/hello/src/main.rs
+++ ch20/hello/src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
 };
 // 3658
 fn main() {
@@ -28,7 +28,13 @@ fn handle_connection(mut stream: TcpStream) {
 
         stream.write_all(response.as_bytes()).unwrap();
     } else {
-        // some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("ch20/hello/404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 
 }
